5d366d8fec1cb6f87ec246b73b5e3f8058e45ee8
Bar Angel <bar.ang16@gmail.com>
2024-03-09 10:15:20 +0000

    move.cpp: taking care of checking

--- move.cpp
+++ move.cpp
@@ -1,5 +1,9 @@
 #include "move.h"
 
+#define PIECE_SELECTED(select) (BOARD2(*((select)->board), (select)->pos) != NO_PIECE)
+
+Selection select_tile_ignore_check(Board *board, int row, int col);
+
 bool select_update(Selection *sel, Board *board, Square c, Piece piece) {
     auto pid = BOARD2(*board, c);
     if (pid == NO_PIECE)
@@ -22,19 +26,69 @@ bool is_move_possible(Selection select, int row, int col) {
     return false;
 }
 
-Board move_selected_piece(Selection select, int row, int col) {
-    Board board = *select.board;
-    BOARD(board, row, col) = BOARD(board, select.pos.row, select.pos.col);
+Board move_selected_piece(Selection *select, int row, int col) {
+    Board board = *select->board;
+    if (!PIECE_SELECTED(select))
+        return board;
+    BOARD(board, row, col) = BOARD2(board, select->pos);
+    BOARD2(board, select->pos) = NO_PIECE;
+
+    // pawn promotion
+    if (row == 0 || row == 7) {
+        auto pid = BOARD(board, row, col);
+        if (board.pieces[pid].type == PIECE_PAWN)
+            board.pieces[pid].type = PIECE_QUEEN;
+    }
+    
     return board;
 }
 
-void delete_possible_moves_due_to_check(Selection *select) {
-    return;
+Piece get_selected_piece(Selection *select) {
+    auto pid = BOARD2(*select->board, select->pos);
+    if (pid == NO_PIECE)
+        return {.type = PIECE_NONE, .player = PLAYER_NONE};
+    return select->board->pieces[pid];
 }
 
-Selection select_tile(Board *board, int row, int col) {
-    Selection sel;
+int checking_pieces(pid *checking, Board *board, Player player) {
+    int count = 0;
+    for (int i = 0; i < NUM_ROWS; i++)
+        for (int j = 0; j < NUM_COLS; j++) {
+            auto pid = BOARD(*board, i, j);
+            if (pid != NO_PIECE && board->pieces[pid].player == OPPONENT_OF(player)) {
+                printf("[%d %d %d]\n", i ,j, board == NULL);
+                auto select = select_tile_ignore_check(board, i, j);
+                for (int t = 0; t < select.num_threatened_pieces; t++) {
+                    if (board->pieces[select.threatened_pieces[t]].type == PIECE_KING) {
+                        if (checking != NULL)
+                            checking[count++] = pid;
+                        else
+                            count++;
+                    }
+                }
+            }
+        }
+    return count;
+}
+
+void delete_possible_moves_due_to_check(Selection *select) {
+    auto player = get_selected_piece(select).player;
+
+    if (checking_pieces(NULL, select->board, player) == 0)
+        return;
     
+    for (int i = 0; i < select->num_possible_moves; i++) {
+        Board hypo_board = move_selected_piece(select, select->possible_moves[i].row, select->possible_moves[i].col);
+        if (checking_pieces(NULL, &hypo_board, player) > 0) {
+            select->possible_moves[i] = NO_SQUARE;
+        }
+    }
+
+}
+
+Selection select_tile_ignore_check(Board *board, int row, int col) {
+    Selection sel;
+
     sel.board = board;
     sel.pos.row = row;
     sel.pos.col = col;
@@ -45,7 +99,6 @@ Selection select_tile(Board *board, int row, int col) {
     if (pid == NO_PIECE)
         return select_none;
     auto piece = board->pieces[pid];
-
     switch (piece.type) {
         case PIECE_KING: {
             for (int i = -1; i <= 1; i++)
@@ -84,7 +137,7 @@ Selection select_tile(Board *board, int row, int col) {
                     if (i == 0 && j == 0) continue;
                     if (piece.type == PIECE_BISHOP && (i == 0 || j == 0)) continue;
                     if (piece.type == PIECE_ROOK && !(i == 0 || j == 0)) continue;
-                    
+
                     for (int v = 1; v < NUM_ROWS; v++) {
                         Square sq = {.row = row + i * v, .col = col + j * v};
                         if (!IN_BOUNDS(sq) || select_update(&sel, board, sq, piece))
@@ -127,8 +180,11 @@ Selection select_tile(Board *board, int row, int col) {
         default:
             return select_none;
     }
-
-    delete_possible_moves_due_to_check(&sel);
-    
     return sel;
+
+}
+Selection select_tile(Board *board, int row, int col) {
+    Selection select = select_tile_ignore_check(board, row, col);
+    delete_possible_moves_due_to_check(&select);
+    return select;
 }
